cc4244641b1d9ab3fa567171338404f71ab1f801
lucasjanta <lucastyle2014@gmail.com>
2023-06-24 12:12:32 -0300

    focus and disable inputs

--- codigo.js
+++ codigo.js
@@ -4,23 +4,25 @@ const botaosalvar = document.querySelector('#botaosalvar');
 const botaomais = document.querySelector('#botaomais');
 
 function apertarEspaco(inputcounter) {
+        inputs[inputcounter - 1].disabled = true;
         var newElement = document.createElement('input');
         newElement.type = 'text';
         newElement.id = `p${inputcounter}`;
         divinputs.appendChild(newElement);
+        newElement.focus();
         newElement.addEventListener('keydown', (event) => {
             if (event.code === 'Space') {
                 apertarEspaco(inputs.length);
             }
         })
-    
+
     inputs = document.querySelectorAll('input[type="text"]');
 }
 
 
 inputs[inputs.length - 1].addEventListener('keydown', (event) => {
     if (event.code === 'Space') {
-
+        
         apertarEspaco(inputs.length);
         console.log(inputs);
     }
