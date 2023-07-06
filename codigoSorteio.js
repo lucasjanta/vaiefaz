a1a0851e13f795dff92341680174cb842d24c7ab
lucasjanta <lucastyle2014@gmail.com>
2023-07-06 11:15:04 -0300

    Sorteio com animação

--- codigoSorteio.js
+++ codigoSorteio.js
@@ -1,6 +1,7 @@
 const atividades = JSON.parse(localStorage.getItem("jsonUnico"));
-const palavras = document.querySelector(".palavras");
-const verbosa = document.querySelector(".verbos");
+const campoVerbos = document.querySelector(".verbos");
+const campoPalavras = document.querySelector(".palavras");
+const botaoDice = document.querySelector(".botaogerador");
 
 
 console.log(atividades);
@@ -13,6 +14,67 @@ for (let i = 0; i < atividades.length; i++) {
 
 console.log(verbos);
 
+function selecionarVerbo() {
+    campoVerbos.innerHTML = "";
+    var verbo = verbos[Math.floor(Math.random() * verbos.length)];
+    var newVerbo = document.createElement("h3");
+    newVerbo.textContent = verbo;
+    campoVerbos.appendChild(newVerbo);
+    return verbo;
+}
+
+function selecionarPalavra(verbo) {
+    campoPalavras.innerHTML = "";
+    for (let i = 0; i < atividades.length; i++) {
+        if (verbo === atividades[i].verbo) {
+            var palavrasPossiveis = [];
+            for (let u = 0; u < atividades[i].palavras.length; u++) {
+                palavrasPossiveis.push(atividades[i].palavras[u]);
+            }
+        }
+    }
+    var palavra = palavrasPossiveis[Math.floor(Math.random() * palavrasPossiveis.length)];
+    var newPalavra = document.createElement("h3");
+    newPalavra.textContent = palavra;
+    campoPalavras.appendChild(newPalavra);
+    return palavra;
+}
+
+/* function selecionarPalavra(verbo) {
+    var palavra = atividades[Math.floor(Math.random() * atividades.length)].palavras[Math.floor(Math.random() * atividades[i].palavras.length)];
+    campoPalavras.textContent = palavra;
+    return palavra;
+} */
+
+botaoDice.addEventListener("click", () => {
+    let contador = 0;
+    const intervaloVerbo = setInterval(() => {
+        contador++;
+        selecionarVerbo();
+        if (contador >= 15) {
+            clearInterval(intervaloVerbo);
+            var verbo = selecionarVerbo();
+            let contador2 = 0;
+            const intervaloPalavra = setInterval(() => {
+                contador2++;
+                selecionarPalavra(verbo);
+                if (contador2 >= 15) {
+                    clearInterval(intervaloPalavra);
+                }
+            }, 100)
+        }
+    }, 100);
+    /* const intervaloPalavra = setInterval(selecionarPalavra, 100); */
+    /* var verbo = selecionarVerbo();
+    var palavra = selecionarPalavra(verbo); */
+
+    
+    
+})
+
+
+
+
 /* function gerarCorVibrante() {
     var hue = Math.floor(Math.random() * 360); // Componente vermelho (0-255)
     var saturation = "100%"; // Componente verde (0-255)
@@ -22,24 +84,11 @@ console.log(verbos);
     return cor;
   } */
 
-function repetiçãoLista(quantidadeElementos){
-    //copiar elemento sliderPalavras
-    if (quantidadeElementos === 4) {
-        //remover o primeiro elemento
-        document.querySelectorAll(".sliderPalavras")[0].remove();
-    } else if (quantidadeElementos < 4) {
-        var sliderauxiliar = sliderPalavras.cloneNode(true);
-        /* sliderauxiliar.classList.add("sliderPalavras"); */
-        palavras.appendChild(sliderauxiliar);
-    }
-    
 
-}
   
-function mostrarAtividades(){
+/* function mostrarAtividades(){
     var arraydeatividades = sliderPalavras.childNodes;
-    /* sliderPalavras.childNodes[1].getBoundingClientRect().x;
-    Posição X dos elementos*/
+   
     var larguraTela = window.innerWidth;
     var xminimo = (larguraTela / 100) * 15;
     var xmaximo = (larguraTela / 100) * 85;
@@ -71,7 +120,7 @@ function mostrarAtividades(){
 
     
     
-}
+} */
 
-mostrarAtividades();
+
 
